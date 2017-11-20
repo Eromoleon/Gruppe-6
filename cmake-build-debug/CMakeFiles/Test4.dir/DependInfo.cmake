3353f3758ea126cff30baceda86624c3b4811fa1
Eromoleon <tamasmagyar123@gmail.com>
2017-11-21 00:33:34 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/Test4.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Dokumentumok/MscI_offline/Clion__PROJECTS/Test4/main.cpp" "D:/Dokumentumok/MscI_offline/Clion__PROJECTS/Test4/cmake-build-debug/CMakeFiles/Test4.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/UOCV/build"
+  "C:/UOCV/include"
+  "C:/UOCV/include/opencv"
+  "C:/UOCV/modules/core/include"
+  "C:/UOCV/modules/flann/include"
+  "C:/UOCV/modules/imgproc/include"
+  "C:/UOCV/modules/ml/include"
+  "C:/UOCV/modules/objdetect/include"
+  "C:/UOCV/modules/photo/include"
+  "C:/UOCV/modules/video/include"
+  "C:/UOCV/modules/imgcodecs/include"
+  "C:/UOCV/modules/shape/include"
+  "C:/UOCV/modules/videoio/include"
+  "C:/UOCV/modules/highgui/include"
+  "C:/UOCV/modules/superres/include"
+  "C:/UOCV/modules/ts/include"
+  "C:/UOCV/modules/features2d/include"
+  "C:/UOCV/modules/calib3d/include"
+  "C:/UOCV/modules/stitching/include"
+  "C:/UOCV/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Test4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test4.dir/main.cpp.obj"
+  "Test4.pdb"
+  "Test4.exe"
+  "Test4.exe.manifest"
+  "libTest4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
